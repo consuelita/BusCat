e38d8f77ff07c4f3ec385e65adae47a46cfc929b
consuelita <adelita@operatorfoundation.org>
2023-07-26 19:15:48 -0500

    Initial Commit

---
+++ BusCat/BusCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusCatApp.swift
+//  BusCat
+//
+//  Created by Mafalda on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BusCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
